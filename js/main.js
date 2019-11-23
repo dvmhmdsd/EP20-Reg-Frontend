13f68fd7f45dbb98a1ca1b5797f91decb57295e0
mohamedsaad <mahameds19999@gmail.com>
2019-11-23 23:41:48 +0200

    launch

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
 //Language specific things
-const ar = {
-  thanks      : "<p>شكرا لتسجيلك، سنقوم بالتواصل معك قريبا لتأكيد ميعاد المقابلة </p>",
-  error       : "<p>حدث خطأ ما، حاول مرةأخرى</p>",
-  fill        : "فضلا املأ هذا الفراغ",
-  validEmail  : "أدخل بريد الكتروني صحيح",
-  validPhone  : "أدخل رقم موبايل صحيح",
-  fill2       : "هذا الفراغ لا بد أن يملأ بما لا يقل عن كلمتين",
-  answer      : "أجب عن هذه الأسئلة",
-  location    : '../ar/location.html'
-};
 
 const en = {
-  thanks      : "<p>Thank you for your registration, We will call you soon to confirm your appointment.</p>",
-  error       : "<p>An error occured, please try again.</p>",
-  fill        : "Please fill in this field",
-  validEmail  : "Please enter a valid email",
-  validPhone  : "Enter a valid phone number",
-  fill2       : "This field must be at least 2 words",
-  answer      : "Answer the questions",
-  location    : '../location.html'
+  thanks: "<p>Thank you for your registration, We will call you soon to confirm your appointment.</p>",
+  error: "<p>An error occured, please try again.</p>",
+  fill: "Please fill in this field",
+  validEmail: "Please enter a valid email",
+  validPhone: "Enter a valid phone number",
+  fill2: "This field must be at least 2 words",
+  answer: "Answer the questions",
+  location: '../location.html'
 }
 
 
@@ -58,30 +48,30 @@ formFields.forEach(field => {
 
   // validate on typing
   field.addEventListener("keydown", validation);
-  
+
   // validate on change
   field.addEventListener("change", validation);
-  
+
   //   save data on reload
-  window.addEventListener('beforeunload', function() {
+  window.addEventListener('beforeunload', function () {
     localStorage.setItem(field.name, field.value);
   })
 
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     // get the value from local storage
     let val = localStorage.getItem(field.name)
     if (val !== null) field.value = val;
   })
-  
+
   if (field.getAttribute("type") == "submit") {
-    field.addEventListener("focus", function() {
+    field.addEventListener("focus", function () {
       validateEmpty(this)
-    });  
+    });
   }
 
   // scroll to show the input on focus in mobile
   if (window.innerWidth == 768) {
-    field.addEventListener("focus", function() {
+    field.addEventListener("focus", function () {
       document.body.scrollTop = this.offsetTop;
     });
   }
@@ -93,7 +83,7 @@ let error = 1;
 // select the form
 let regForm = document.querySelector("#regForm");
 
-regForm.addEventListener("submit", function(e) {
+regForm.addEventListener("submit", function (e) {
   let sub_form = {};
   //   validateEmpty the fields
   formFields.forEach(field => {
@@ -107,7 +97,7 @@ regForm.addEventListener("submit", function(e) {
     // disable the submit button
     // document.querySelector("#regForm input[type='submit']").disabled = true;
 
-    // prevent the submition if there's errors
+    // prevent the submission if there's errors
     e.preventDefault();
     formFields.forEach(field => {
       validateEmpty(field);
@@ -121,26 +111,28 @@ regForm.addEventListener("submit", function(e) {
     // the content of the modal
     let modalContent = modal.querySelector(".modal-content");
     // hide when clicking any where except the modal
-    window.onclick = function(e) {
+    window.onclick = function (e) {
       if (e.target == modal) {
         modal.style.display = "none";
       }
     };
     // show the modal
     modal.style.display = "block";
-    
+
     // hide the questions of committees
     document.querySelectorAll('.question').forEach(question => {
       question.style.display = 'none';
     })
-  // show the loading before every request
+    // show the loading before every request
     if (modalContent.querySelector('p')) {
-        modalContent.querySelector('p').style.display = 'none';
+      modalContent.querySelectorAll('p').forEach(p => {
+        p.style.display = 'none';
+      })
     }
     modalContent.querySelector('img').style.display = 'block';
 
     // send data to the api
-    fetch("https://stark-temple-62549.herokuapp.com/register", {
+    fetch("https://vast-brushlands-53011.herokuapp.com/register", {
       method: "post",
       headers: {
         Accept: "application/json, text/plain, */*",
@@ -150,59 +142,28 @@ regForm.addEventListener("submit", function(e) {
     })
       .then(res => {
         // show success message
-        if (location.href.indexOf("ar") > 0) {
-            modalContent.querySelector('img').style.display = 'none';
-            modalContent.querySelectorAll('p').forEach(p => {
-                p.style.display = 'none';
-            })
-            modalContent.insertAdjacentHTML("beforeend", `<p>${ar.thanks}</p>`)
-              
-        } else {
-            modalContent.querySelector('img').style.display = 'none';
-            modalContent.querySelectorAll('p').forEach(p => {
-                p.style.display = 'none';
-            })
-            modalContent.insertAdjacentHTML("beforeend", `<p>${en.thanks}</p>`)
-        }
-              
-        //Redirect to the location-showing page
-        setTimeout(() => {
-            if (location.href.indexOf("ar") > 0) {
-                location.replace(ar.location)
-            } else {
-                location.replace(en.location)
-            }
-        }, 2500);
-              
-        //Redirect to the location-showing page
-        setTimeout(() => {
-          location.replace('/register/location.html')
-        }, 2500);
-
+        modalContent.querySelector('img').style.display = 'none';
+        modalContent.querySelectorAll('p').forEach(p => {
+          p.style.display = 'none';
+        })
+        modalContent.insertAdjacentHTML("beforeend", `<p>${en.thanks}</p>`)
 
         // Reset the form on success
         formFields.forEach(field => {
           if (field.getAttribute("type") !== "submit" && field.offsetParent !== null) {
-              field.value = "";
-              field.classList.remove("valid");
-              field.classList.remove("invalid");
+            field.value = "";
+            field.classList.remove("valid");
+            field.classList.remove("invalid");
           }
         });
       })
       .catch(err => {
-          if (location.href.indexOf("ar") > 0) {
-            modalContent.querySelector('img').style.display = 'none';
-            modalContent.querySelectorAll('p').forEach(p => {
-                p.style.display = 'none';
-            })
-            modalContent.insertAdjacentHTML("beforeend", `<p>${ar.error}</p>`)
-          } else {
-            modalContent.querySelector('img').style.display = 'none';
-            modalContent.querySelectorAll('p').forEach(p => {
-                p.style.display = 'none';
-            })
-            modalContent.insertAdjacentHTML("beforeend", `<p>${en.error}</p>`)
-          }
+        tent.insertAdjacentHTML("beforeend", `<p>${ar.error}</p>`)
+        modalContent.querySelector('img').style.display = 'none';
+        modalContent.querySelectorAll('p').forEach(p => {
+          p.style.display = 'none';
+        })
+        modalContent.insertAdjacentHTML("beforeend", `<p>${en.error}</p>`)
       })
   }
 });
@@ -230,7 +191,7 @@ function validateEmpty(field) {
 
         error += 1;
         // insert error message after the empty field
-        message(field, (location.href.indexOf("ar") > 0 ) ? ar.fill : en.fill);
+        message(field, (location.href.indexOf("ar") > 0) ? ar.fill : en.fill);
       }
     }
   }
@@ -247,7 +208,7 @@ function validateEmail(field) {
 
       error += 1;
 
-      message(field, (location.href.indexOf("ar") > 0 ) ? ar.validEmail : en.validEmail);
+      message(field, en.validEmail);
     }
   } else {
     field.classList.add("valid");
@@ -277,7 +238,7 @@ function validatePhone(field) {
 
       error += 1;
 
-      message(field, (location.href.indexOf("ar") > 0 ) ? ar.validPhone : en.validPhone);
+      message(field, en.validPhone);
     }
   } else {
     field.classList.add("valid");
@@ -306,7 +267,7 @@ function validateTextArea(field) {
 
       error += 1;
 
-      message(field, (location.href.indexOf("ar") > 0 ) ? ar.fill2 : en.fill2);
+      message(field, en.fill2);
     }
   } else {
     field.classList.add("valid");
@@ -335,7 +296,7 @@ let committeeSelect = document.querySelector("select#committee");
 let committeeSelect2 = document.querySelector("select#committee2");
 
 // handle the change event in the select box
-committeeSelect.addEventListener("change", function() {
+committeeSelect.addEventListener("change", function () {
   // remove all other questions
   document.querySelectorAll(".question").forEach(question => {
     question.style.display = "none";
@@ -344,26 +305,26 @@ committeeSelect.addEventListener("change", function() {
   if (document.querySelector(`#${this.value}`)) {
     document.querySelector(`#${this.value}`).style.display = "block";
   }
-  
-//   remove the chosen option from 2nd preference
-    for (let i = 0; i < committeeSelect2.children.length; i++) {
-        if (committeeSelect2.children[i].value == this.value) {
-          committeeSelect2.children[i].style.display = 'none'
-        } else {
-          committeeSelect2.children[i].style.display = 'block'
-        }
+
+  //   remove the chosen option from 2nd preference
+  for (let i = 0; i < committeeSelect2.children.length; i++) {
+    if (committeeSelect2.children[i].value == this.value) {
+      committeeSelect2.children[i].style.display = 'none'
+    } else {
+      committeeSelect2.children[i].style.display = 'block'
     }
+  }
 });
 
 // show the input for 'other' choice appear on selecting the option
 let universitySelect = document.querySelector("select#university");
 
 // handle the change event in the select box
-universitySelect.addEventListener("change", function() {
+universitySelect.addEventListener("change", function () {
   if (this.value == "other") {
     showOtherOption(this);
   } else {
-      this.parentElement.nextElementSibling.style.display = 'none'
+    this.parentElement.nextElementSibling.style.display = 'none'
   }
 });
 
@@ -371,11 +332,11 @@ universitySelect.addEventListener("change", function() {
 let facultySelect = document.querySelector("select#faculty");
 
 // handle the change event in the select box
-facultySelect.addEventListener("change", function() {
+facultySelect.addEventListener("change", function () {
   if (this.value == "other") {
     showOtherOption(this);
   } else {
-      this.parentElement.nextElementSibling.style.display = 'none'
+    this.parentElement.nextElementSibling.style.display = 'none'
   }
   // show the "year selectbox"
   document.querySelector(".year").style.display = "block";
@@ -385,16 +346,16 @@ facultySelect.addEventListener("change", function() {
     document.querySelector(".eng-dept").style.display = "none";
   }
 
-  
+
   if (this.value == "pharmacyeuticals") {
     yearCount(5);
   } else if (this.value == "applied arts" || this.value == "Engineering") {
     yearCount(4);
-    
+
     let option = document.createElement("option");
     option.innerHTML = "preparatory year";
     option.value = "preparatory";
-    yearSelect.appendChild(option);
+    yearSelect.insertBefore(option, yearSelect.children[1])
   } else if (this.value == "medicine") {
     yearCount(6);
   } else {
@@ -406,7 +367,7 @@ let yearSelect = document.querySelector("select#year");
 // fill the select with the corresponding year count
 function yearCount(count) {
   // TODO: remove all children of select before choose
-  while(yearSelect.firstElementChild.nextElementSibling) {
+  while (yearSelect.firstElementChild.nextElementSibling) {
     yearSelect.firstElementChild.nextElementSibling.remove();
   }
 
@@ -444,7 +405,7 @@ function showOtherOption(targetSelect) {
   let otherInput = otherOption.querySelector("input");
 
   // set the option value to input value
-  otherInput.addEventListener("change", function() {
+  otherInput.addEventListener("change", function () {
     // create an option with value of this input
     let option = document.createElement("option");
     // add the attribute "selected" to it
@@ -456,24 +417,20 @@ function showOtherOption(targetSelect) {
   });
 }
 
-let howSelect = document.querySelector("select#how");
+// let howSelect = document.querySelector("select#how");
 
-howSelect.addEventListener('change', function() {
-  if (this.value == "Social media") {
-    document.querySelector(".social").style.display = "block";
-  } else {
-    document.querySelector(".social").style.display = "none";
-  }
-})
+// howSelect.addEventListener('change', function () {
+//   if (this.value == "Social media") {
+//     document.querySelector(".social").style.display = "block";
+//   } else {
+//     document.querySelector(".social").style.display = "none";
+//   }
+// })
 
 // prevent scrolling on load
-window.addEventListener("load", function() {
-<<<<<<< HEAD
-  if (location.href.indexOf("ar") > 0) {
-    btnHeader.innerHTML = ar.answer;
-  } else {
-    btnHeader.innerHTML = en.answer;
-  }
+window.addEventListener("load", function () {
+
+  btnHeader.innerHTML = en.answer;
 
   btnHeader.disabled = false;
 
@@ -482,7 +439,7 @@ window.addEventListener("load", function() {
 });
 
 // prevent scrolling on load
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
   // scroll to top on load
   window.scrollTo(0, 0);
 
@@ -492,7 +449,7 @@ window.addEventListener("DOMContentLoaded", function() {
 // scroll to form on clicking on the button
 let btnHeader = document.querySelector(".btn-header");
 
-btnHeader.addEventListener("click", function() {
+btnHeader.addEventListener("click", function () {
   // make the body scrollable back
   document.body.style.overflow = "auto";
   document.body.style.height = 2100 + "px";
@@ -505,7 +462,6 @@ btnHeader.addEventListener("click", function() {
   // animate the form
   document.querySelector('.card').classList.add('anime-form');
   document.querySelector('main .container').classList.remove('contain')
-<<<<<<< HEAD
 });
 
 
@@ -513,12 +469,12 @@ btnHeader.addEventListener("click", function() {
 termsBtn = document.querySelector('#terms-click');
 termsModal = document.querySelector('#tAc');
 
-termsBtn.addEventListener('click', function(e) {
+termsBtn.addEventListener('click', function (e) {
   e.preventDefault();
   termsModal.style.display = 'block'
 })
 // hide when clicking any where except the modal
-window.onclick = function(e) {
+window.onclick = function (e) {
   if (e.target == termsModal) {
     termsModal.style.display = "none";
   }
@@ -530,12 +486,12 @@ let termsCheck = document.querySelector('#terms-check');
 let submitBtn = document.querySelector("#regForm input[type='submit']");
 submitBtn.disabled = true;
 
-termsCheck.addEventListener('change', function() {
+termsCheck.addEventListener('change', function () {
   if (this.checked) {
     formFields.forEach(field => {
-        validateEmpty(field);
+      validateEmpty(field);
     });
-    
+
     submitBtn.disabled = false;
   } else {
     submitBtn.disabled = true;
